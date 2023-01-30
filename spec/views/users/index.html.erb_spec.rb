93557dd07fee917816892d81e35c868583500f06
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-30 09:57:04 -0700

    update users/index_spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,29 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "users/index", type: :view do
+RSpec.describe 'users/index', type: :view do
   before(:each) do
     assign(:users, [
-      User.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        email: "Email",
-        password_digest: "Password Digest"
-      ),
-      User.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        email: "Email",
-        password_digest: "Password Digest"
-      )
-    ])
+             User.create!(
+               first_name: 'First Name',
+               last_name: 'Last Name',
+               email: 'Email',
+               password_digest: 'Password Digest'
+             )
+           ])
   end
 
-  it "renders a list of users" do
+  it 'renders a list of users' do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("First Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Last Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Email".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Password Digest".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('First Name'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('Last Name'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('Email'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('Password Digest'.to_s), count: 1
   end
 end
