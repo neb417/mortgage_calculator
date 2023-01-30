aeaf24fbad232c84c3325f95b394fb234c892767
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-30 11:04:37 -0700

    add login form to landing and sign up link.

--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'landing index' do
-  it 'returns as successful response' do
+  before :each do
     visit root_path
-
+  end
+  it 'returns as successful response' do
     expect(page).to have_content('Welcome to the Page')
+  end
+
+  it 'has form to login' do
+    expect(page).to have_content('Email')
+    expect(page).to have_content('Password')
+    expect(page).to have_button('Login')
+  end
+
+  it 'has link to register new user' do
+    expect(page).to have_content("Don't have an account?")
+    expect(page).to have_link('Sign Up')
+
+    click_on 'Sign Up'
+
+    expect(current_path).to eq new_user_path
   end
 end
